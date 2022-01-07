976d59eafd6de2ef0b299d71407ea017e01a4ecd
priyanshu10 <priyanshuparate@gmail.com>
2022-01-07 17:30:40 +0530

    intial react native app

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import { useState } from "react";
 
 export default function App() {
+  const [couter, setCouter] = useState(0);
+  const increase = () => {
+    setCouter((x) => x + 1);
+  };
+  const decriese = () => {
+    setCouter((x) => x - 1);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{couter}</Text>
+      <Button title="counter +" onPress={increase} />
+      <Button title="counter -" onPress={decriese} />
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
